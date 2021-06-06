bcd7e6e44bb6bf3abc0bec7e78a3d20383226c3e
from97 <skm1104@daum.net>
2021-06-06 14:09:29 +0900

    parsing.c: count_words() OK

--- seomoon_parsing/parsing.c
+++ seomoon_parsing/parsing.c
@@ -6,11 +6,12 @@
 /*   By: seomoon <seomoon@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 18:25:09 by seomoon           #+#    #+#             */
-/*   Updated: 2021/06/05 19:00:13 by seomoon          ###   ########.fr       */
+/*   Updated: 2021/06/06 14:09:11 by seomoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+#include<stdio.h>
 
 # define DOUBLE_QUATER 2
 # define SINGLE_QUATER 1
@@ -31,49 +32,58 @@ char		is_quater(char c)
 	return (0);
 }
 
-int		skip_string_arg(char *str, int *i, char quater)
-{
-
-}
-
 int		count_words(char *str, char sep)
 {
 	int	i;
 	int	count;
-	int	flag;
-	int	quater;
+	char	quater;
 
 	i = 0;
 	count = 0;
-	flag = 0;
 	while (str[i])
 	{
-		if (quater = is_quater(str[i]))
+		if (is_quater(str[i]))
 		{
-			skip_string_arg(str, &i, quater);
-			count++;
+			quater = str[i++];
+			while (str[i] && str[i] != quater)
+				i++;
 		}
+		if (!is_seperator(str[i], sep) &&
+				(is_seperator(str[i + 1], sep) || str[i + 1] == '\0'))
+			count += 1;
 		i++;
 	}
+	return (count);
 }
 
-char		**ft_split(char *str, char sep)
+int		ft_split(char *str, char sep)
 {
-	char	**split_strs;
-	int	i;
-	int	j;
+	//char	**split_strs;
+	//int	i;
+	//int	j;
 	int	count;
 
 	count = count_words(str, sep);
-}
+	/*
+	split_strs = malloc(sizeof(char *) * (count + 1));
+	if (!split_strs)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
 
+	}
+	*/
+	return (count);
+}
+/*
 int		parse_input(t_cmd *cmd_head, char *input)
 {
-	/*
+	
 	1. input(입력된 커맨드라인)을 스페이스 기준으로 쪼개기
 	2. 이 때, 따옴표 안의 스페이스는 문자열로 처리함
 	3. 쪼갠 인자들을 2차원 배열(문자열 포인터 배열)에 저장하기
-	*/
+	
 	int	i;
 
 	i = 0;
@@ -82,12 +92,17 @@ int		parse_input(t_cmd *cmd_head, char *input)
 		
 	}
 }
+*/
 
 int		main()
 {
-	t_cmd	cmd_head;
+	//t_cmd	cmd_head;
 	char	*input;
+	int result;
 
 	input = "ls        -la > a         >> b | grep \"          a\" ; pwd";
-	parse_input(&cmd_head, input);
+	//parse_input(&cmd_head, input);
+	result = ft_split(input, ' ');
+	printf("%s\n", input);
+	printf("%d\n", result);
 }
