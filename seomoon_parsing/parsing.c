558541e33c4e4e64ae41654f8cae456c012dd4ad
from97 <skm1104@daum.net>
2021-06-06 14:42:44 +0900

    parsing.c 일부 파싱 성공

--- seomoon_parsing/parsing.c
+++ seomoon_parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: seomoon <seomoon@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 18:25:09 by seomoon           #+#    #+#             */
-/*   Updated: 2021/06/06 14:10:41 by seomoon          ###   ########.fr       */
+/*   Updated: 2021/06/06 14:41:39 by seomoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,16 +35,21 @@ int		count_words(char *str, char sep)
 	int	i;
 	int	count;
 	char	quater;
+	int	len;
 
 	i = 0;
 	count = 0;
+	len = 0;
 	while (str[i])
 	{
 		if (is_quater(str[i]))
 		{
 			quater = str[i++];
 			while (str[i] && str[i] != quater)
+			{
+				len++;
 				i++;
+			}
 		}
 		if (!is_seperator(str[i], sep) &&
 				(is_seperator(str[i + 1], sep) || str[i + 1] == '\0'))
@@ -54,25 +59,63 @@ int		count_words(char *str, char sep)
 	return (count);
 }
 
-int		ft_split(char *str, char sep)
+int		push_str(char **split_strs, char *str, char sep, int i)
 {
-	//char	**split_strs;
-	//int	i;
-	//int	j;
+	int	j;
+	int	len;
+
+	len = 0;
+	while (!is_seperator(str[len], sep) && str[len])
+		len++;
+	split_strs[i] = malloc((len + 1) * sizeof(char));
+	if (split_strs[i] == 0)
+		return (0);
+	j = 0;
+	while (!is_seperator(*str, sep) && *str)
+	{
+		split_strs[i][j] = *str;
+		j++;
+		str++;
+	}
+	split_strs[i][j] = '\0';
+	return (j);
+}
+
+char		**ft_split(char *str, char sep)
+{
+	char	**split_strs;
+	int	i;
+	int	j;
 	int	count;
+	char	quater;
 
 	count = count_words(str, sep);
-	/*
 	split_strs = malloc(sizeof(char *) * (count + 1));
 	if (!split_strs)
 		return (NULL);
 	i = 0;
-	while (str[i])
+	while (*str)
 	{
+		if (is_quater(*str)) //quater
+		{
+			quater = *str;
+			str++;
+			j = push_str(split_strs, str, quater, i);
+			str += j;
+			i++;
+		}
+		else if (is_seperator(*str, sep)) //seperator
+			str++;
+		else //else
+		{
+			j = push_str(split_strs, str, sep, i);
+			str += j;
+			i++;
+		}
 
 	}
-	*/
-	return (count);
+	split_strs[i] = NULL;
+	return (split_strs);
 }
 /*
 int		parse_input(t_cmd *cmd_head, char *input)
@@ -96,11 +139,18 @@ int		main()
 {
 	//t_cmd	cmd_head;
 	char	*input;
-	int result;
+	char	**split_strs;
+	int	i;
 
 	input = "ls        -la > a         >> b | grep \"          a\" ; pwd";
 	//parse_input(&cmd_head, input);
-	result = ft_split(input, ' ');
+	split_strs = ft_split(input, ' ');
+	i = 0;
+	while (split_strs[i])
+	{
+		printf("split_strs[%d]: %s\n", i, split_strs[i]);
+		i++;
+	}
 	printf("%s\n", input);
-	printf("%d\n", result);
+	printf("%d\n", i);
 }
