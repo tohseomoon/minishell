34e2fcf0e8a29d9c0964472b9564ded9120d8db4
오태경 <toh@otaegyeong-ui-MacBookPro.local>
2021-06-07 21:47:54 +0900

    20210607_헤더수정

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: toh <toh@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 18:05:13 by seomoon           #+#    #+#             */
-/*   Updated: 2021/06/04 20:03:24 by toh              ###   ########.fr       */
+/*   Updated: 2021/06/07 21:47:22 by toh              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,8 @@
 # define BUFFER_SIZE 1024
 # define STDIN 0
 # define STDOUT 1
-# define LEFT_REDIR 5
-# define RIGHT_REDIR 6
-# define DOUBLE_REDIR 7
-# define PIPE 8
+# define END 0
+# define PIPE 1
 
 typedef struct      s_env
 {
@@ -38,31 +36,36 @@ typedef struct      s_cmd
     int             pipe[2];
     int             fd_in;
     int             fd_out;
-    int             type;
+    int             flag;
     struct s_cmd    *next;
 }                   t_cmd;
 
+typedef struct      s_data
+{
+    struct s_cmd    *cmd_head;
+    struct s_env    *env_head;
+}                   t_data;
 
-char	            *ft_strldup(const char *s1, int n);
+char                *ft_strldup(const char *s1, int n);
 
 int                 parse_env(t_env *env_head, char **env);
 void                print_env(t_env *env_head);
 
-char	            *ft_strjoin_free_s1(char **s1, char *s2);
-int		            ft_strchr_index(const char *s, int c);
+char                *ft_strjoin_free_s1(char **s1, char *s2);
+int                 ft_strchr_index(const char *s, int c);
 
-size_t	            ft_strlen(const char *s);
-size_t	            ft_strlcpy(char *dest, const char *src, size_t destsize);
-char	            *ft_strdup(const char *s1);
+size_t              ft_strlen(const char *s);
+size_t              ft_strlcpy(char *dest, const char *src, size_t destsize);
+char                *ft_strdup(const char *s1);
 
-int			        get_next_line(int fd, char **line);
+int                 get_next_line(int fd, char **line);
 
-void	            ft_putchar_fd(char c, int fd);
-void	            ft_putstr_fd(char *s, int fd);
-int		            ft_strcmp(char *s1, char *s2);
+void                ft_putchar_fd(char c, int fd);
+void                ft_putstr_fd(char *s, int fd);
+int                 ft_strcmp(char *s1, char *s2);
 
-void	            ft_putchar_fd(char c, int fd);
-void	            ft_putstr_fd(char *s, int fd);
-int		            ft_strcmp(char *s1, char *s2);
+void                ft_putchar_fd(char c, int fd);
+void                ft_putstr_fd(char *s, int fd);
+int                 ft_strcmp(char *s1, char *s2);
 
 #endif
