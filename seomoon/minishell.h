976bcc9a585274797d7d73179618b439060f78d7
from97 <skm1104@daum.net>
2021-06-13 21:17:37 +0900

    디렉터리 구조 변경

--- seomoon/minishell.h
+++ seomoon/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toh <toh@student.42seoul.kr>               +#+  +:+       +#+        */
+/*   By: seomoon <seomoon@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/04 18:05:13 by seomoon           #+#    #+#             */
-/*   Updated: 2021/06/10 21:21:22 by seomoon          ###   ########.fr       */
+/*   Created: 2021/06/12 20:28:55 by seomoon           #+#    #+#             */
+/*   Updated: 2021/06/13 12:56:41 by seomoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,59 +15,52 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include<stdio.h>
 
+# define STDIN	0
+# define STDOUT	1
 # define BUFFER_SIZE 1024
-# define STDIN 0
-# define STDOUT 1
-# define END 0
-# define PIPE 1
 
-typedef struct      s_env
+# define S_QUOTE '\''
+# define D_QUOTE '\"'
+# define ESCAPE  '\\'
+
+typedef struct		s_env
 {
-    char            *key;
-    char            *value;
-    struct s_env    *next;
-}                   t_env;
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}					t_env;
 
-typedef struct      s_cmd      
+typedef struct		s_cmd
 {
-    char            **argv;
-    int             argc;
-    int             pipe[2];
-    int             fd_in;
-    int             fd_out;
-    int             flag;
-    struct s_cmd    *next;
-}                   t_cmd;
+	char			**argv;
+	int				argc;
+	int				index;
+	struct s_cmd	*next;
+}					t_cmd;
 
-typedef struct      s_data
+typedef struct		s_data
 {
-    struct s_cmd    *cmd_head;
-    struct s_env    *env_head;
-}                   t_data;
+	struct s_cmd	*cmd_head;
+	struct s_env	*env_head;
+}					t_data;
 
-char                *ft_strldup(const char *s1, int n);
 
+// env
 int                 parse_env(t_env *env_head, char **env);
 void                print_env(t_env *env_head);
 
-char                *ft_strjoin_free_s1(char **s1, char *s2);
-int                 ft_strchr_index(const char *s, int c);
-
+// get_next_line, utils
 size_t              ft_strlen(const char *s);
 size_t              ft_strlcpy(char *dest, const char *src, size_t destsize);
 char                *ft_strdup(const char *s1);
-
+char                *ft_strldup(const char *s1, int n);
+void                ft_putchar_fd(char c, int fd);
+void                ft_putstr_fd(char *s, int fd);
+int                 ft_strcmp(char *s1, char *s2);
+char                *ft_strjoin_free_s1(char **s1, char *s2);
+int                 ft_strchr_index(const char *s, int c);
 int                 get_next_line(int fd, char **line);
-
-void                ft_putchar_fd(char c, int fd);
-void                ft_putstr_fd(char *s, int fd);
-int                 ft_strcmp(char *s1, char *s2);
-
-void                ft_putchar_fd(char c, int fd);
-void                ft_putstr_fd(char *s, int fd);
-int                 ft_strcmp(char *s1, char *s2);
-
-void		parse_input(t_cmd *cmd_head, t_env *env_head, char *input);
 
 #endif
---
+++ seomoon/old/minishell.h
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toh <toh@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/04 18:05:13 by seomoon           #+#    #+#             */
+/*   Updated: 2021/06/10 21:21:22 by seomoon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define BUFFER_SIZE 1024
+# define STDIN 0
+# define STDOUT 1
+# define END 0
+# define PIPE 1
+
+typedef struct      s_env
+{
+    char            *key;
+    char            *value;
+    struct s_env    *next;
+}                   t_env;
+
+typedef struct      s_cmd      
+{
+    char            **argv;
+    int             argc;
+    int             pipe[2];
+    int             fd_in;
+    int             fd_out;
+    int             flag;
+    struct s_cmd    *next;
+}                   t_cmd;
+
+typedef struct      s_data
+{
+    struct s_cmd    *cmd_head;
+    struct s_env    *env_head;
+}                   t_data;
+
+char                *ft_strldup(const char *s1, int n);
+
+int                 parse_env(t_env *env_head, char **env);
+void                print_env(t_env *env_head);
+
+char                *ft_strjoin_free_s1(char **s1, char *s2);
+int                 ft_strchr_index(const char *s, int c);
+
+size_t              ft_strlen(const char *s);
+size_t              ft_strlcpy(char *dest, const char *src, size_t destsize);
+char                *ft_strdup(const char *s1);
+
+int                 get_next_line(int fd, char **line);
+
+void                ft_putchar_fd(char c, int fd);
+void                ft_putstr_fd(char *s, int fd);
+int                 ft_strcmp(char *s1, char *s2);
+
+void                ft_putchar_fd(char c, int fd);
+void                ft_putstr_fd(char *s, int fd);
+int                 ft_strcmp(char *s1, char *s2);
+
+void		parse_input(t_cmd *cmd_head, t_env *env_head, char *input);
+
+#endif
