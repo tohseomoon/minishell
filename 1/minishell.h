92d9cb77c8eb06371448b4750261198678681622
from97 <skm1104@daum.net>
2021-06-15 14:56:52 +0900

    parse 함수 나누기, norm, 헤더

---
+++ 1/minishell.h
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seomoon <seomoon@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/12 20:28:55 by seomoon           #+#    #+#             */
+/*   Updated: 2021/06/15 14:38:03 by seomoon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdio.h> //check
+
+# define STDIN	0
+# define STDOUT	1
+# define BUFFER_SIZE 1024
+
+# define S_QUOTE '\''
+# define D_QUOTE '\"'
+# define ESCAPE  '\\'
+
+typedef struct		s_env
+{
+	char			*key;
+	char			*value;
+	int				equal;
+	struct s_env	*next;
+}					t_env;
+
+typedef struct		s_cmd
+{
+	char			**argv;
+	int				argc;
+	int				index;
+	int				pipe[2];
+	int				fd_in;
+	int				fd_out;
+	struct s_cmd	*next;
+	struct s_cmd	*prev;
+}					t_cmd;
+
+typedef struct		s_data
+{
+	struct s_cmd	*cmd_head;
+	struct s_env	*env_head;
+	char			**path;
+	char			**old_env;
+	char			**new_env;
+	int				return_value;
+	int				running;
+}					t_data;
+
+t_data				g_data;
+
+void				exit_shell(char *message);
+void				parse_command(char *command);
+int					handle_single_quote(t_cmd *curr, char *command);
+int					handle_double_quote(t_cmd *curr, char *command);
+int					handle_symbol(t_cmd *curr, char *command);
+int					is_space(char c);
+int					is_seperator(char c);
+int					is_command_end(char c);
+int					is_symbol(char c);
+int					parse_env(char **env);
+void				print_env(void);
+size_t				ft_strlen(const char *s);
+size_t				ft_strlcpy(char *dest, const char *src, size_t destsize);
+char				*ft_strdup(const char *s1);
+char				*ft_strldup(const char *s1, int n);
+void				ft_putchar_fd(char c, int fd);
+void				ft_putstr_fd(char *s, int fd);
+int					ft_strcmp(char *s1, char *s2);
+char				*ft_strjoin_free_s1(char **s1, char *s2);
+int					ft_strchr_index(const char *s, int c);
+char				*ft_strtrim(char *str);
+
+#endif
---
+++ 1/utils.c
@@ -0,0 +1,166 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toh <toh@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/21 14:39:24 by toh               #+#    #+#             */
+/*   Updated: 2021/06/15 14:16:07 by seomoon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void		ft_putchar_fd(char c, int fd)
+{
+	if (fd < 0)
+		return ;
+	write(fd, &c, 1);
+}
+
+void		ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	while (*s)
+		ft_putchar_fd(*s++, fd);
+}
+
+int			ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if ((unsigned char)s1[i] == (unsigned char)s2[i])
+			i++;
+		else
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
+}
+
+char		*ft_strjoin_free_s1(char **s1, char *s2)
+{
+	int		s1_len;
+	int		s2_len;
+	char	*result;
+
+	if (*s1 == 0 || s2 == 0)
+	{
+		free(*s1);
+		*s1 = 0;
+		return (0);
+	}
+	s1_len = ft_strlen(*s1);
+	s2_len = ft_strlen(s2);
+	result = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (result == 0)
+	{
+		free(*s1);
+		*s1 = 0;
+		return (0);
+	}
+	ft_strlcpy(result, *s1, s1_len + 1);
+	ft_strlcpy(result + s1_len, s2, s2_len + 1);
+	free(*s1);
+	*s1 = 0;
+	return (result);
+}
+
+int			ft_strchr_index(const char *s, int c)
+{
+	int		i;
+
+	i = 0;
+	while (s[i] != (unsigned char)c)
+	{
+		if (s[i] == 0)
+			return (0);
+		i++;
+	}
+	i++;
+	return (i);
+}
+
+size_t		ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+size_t		ft_strlcpy(char *dest, const char *src, size_t destsize)
+{
+	size_t	cnt;
+
+	if (dest == 0 || src == 0)
+		return (0);
+	cnt = 0;
+	while (cnt + 1 < destsize && src[cnt] != 0)
+	{
+		dest[cnt] = src[cnt];
+		cnt++;
+	}
+	if (destsize != 0)
+		dest[cnt] = 0;
+	while (src[cnt])
+		cnt++;
+	return (cnt);
+}
+
+char		*ft_strtrim(char *str)
+{
+	int		begin;
+	int		end;
+	char	*result;
+
+	if (!str)
+		return (NULL);
+	begin = 0;
+	while (str[begin] && is_space(str[begin]))
+		begin++;
+	end = ft_strlen(str + begin);
+	if (end)
+		while (str[begin + end - 1] && is_space(str[begin + end - 1]))
+			end--;
+	result = malloc(sizeof(char) * (end + 1));
+	if (!result)
+		return (NULL);
+	ft_strlcpy(result, (char *)(str + begin), end + 1);
+	return (result);
+}
+
+char		*ft_strdup(const char *s1)
+{
+	int		str_len;
+	char	*str;
+
+	str_len = ft_strlen(s1);
+	str = (char *)malloc(sizeof(char) * (str_len + 1));
+	if (str == 0)
+		return (0);
+	ft_strlcpy(str, s1, str_len + 1);
+	return (str);
+}
+
+char		*ft_strldup(const char *s1, int n)
+{
+	int		str_len;
+	char	*str;
+
+	str_len = ft_strlen(s1);
+	if (n > str_len + 1)
+		return (0);
+	str = (char *)malloc(sizeof(char) * (n + 1));
+	if (str == 0)
+		return (0);
+	ft_strlcpy(str, s1, n);
+	str[n] = 0;
+	return (str);
+}
