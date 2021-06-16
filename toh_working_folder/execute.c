8bfbb033eb7252e4a9029f02c33110b1a3f36743
오태경 <toh@otaegyeong-ui-MacBookPro.local>
2021-06-16 16:17:52 +0900

    toh_20210616_export,cd

---
+++ toh_working_folder/execute.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toh <toh@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/15 14:01:17 by toh               #+#    #+#             */
+/*   Updated: 2021/06/16 16:00:11 by toh              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void		check_pipe(t_cmd *curr)
+{
+	if (curr->heredoc == 1)
+	{
+		dup2(curr->heredoc_pipe[0], 0);
+		if (curr->next != 0 && curr->prev == 0)
+			dup2(curr->pipe[1], 1);
+	}
+	else
+	{
+		if (curr->next != 0 && curr->prev == 0)
+			dup2(curr->pipe[1], 1);
+		else if (curr->next != 0 && curr->prev != 0)
+		{
+			dup2(curr->prev->pipe[0], 0);
+			dup2(curr->pipe[1], 1);
+		}
+		else if (curr->prev != 0 && curr->next == 0)
+			dup2(curr->prev->pipe[0], 0);
+	}
+}
+
+void		execute_cmd_path(t_cmd *curr, char **envp)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		check_pipe(curr);
+		if (curr->fd_in != 0 && curr->heredoc != 1)
+			dup2(curr->fd_in, 0);
+		if (curr->fd_out != 1)
+			dup2(curr->fd_out, 1);
+		if (execve(curr->argv[0], curr->argv, envp) == -1)
+		{
+			printf("bash: %s: %s\n", curr->argv[0], strerror(errno));
+			exit(1);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		close(curr->pipe[1]);
+		g_data.return_value = WEXITSTATUS(status);
+		if (curr->heredoc == 1)
+		{
+			curr->heredoc = 0;
+			close(curr->heredoc_pipe[0]);
+		}
+	}
+}
+
+void		close_file(t_cmd *curr)
+{
+	if (curr->fd_in != 0)
+		close(curr->fd_in);
+	if (curr->fd_out != 1)
+		close(curr->fd_out);
+	if (curr->prev != 0)
+		close(curr->prev->pipe[0]);
+}
+
+void	execute_command(char **envp)
+{
+	t_cmd	*curr;
+	int		i;
+
+	curr = g_data.cmd_head->next;
+	while (curr)
+	{
+		pipe(curr->pipe);
+		if ((i = redirection_open_file(curr)) > 0)
+			printf("bash: %s: No such file or directory\n", curr->argv[i + 1]);
+		else if (check_shell_builtin_fork(curr))
+			builtin_cmd_fork(curr);
+		else if (check_shell_builtin(curr))
+			builtin_cmd(curr);
+		else if (curr->argv[0][0] == '/')
+		{
+			if (find_cmd_absolute_path(curr))
+				execute_cmd_path(curr, envp);
+		}
+		else if (find_cmd_path(curr))
+			execute_cmd_path(curr, envp);
+		else
+			printf("bash : %s: command not found\n", curr->argv[0]);
+		//close_file(curr);
+		curr = curr->next;
+	}
+}
---
+++ toh_working_folder/shell_builtin_fun.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shell_builtin_fun.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toh <toh@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/15 14:48:18 by toh               #+#    #+#             */
+/*   Updated: 2021/06/16 15:02:16 by toh              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int				check_shell_builtin_fork(t_cmd *curr)
+{
+	if (ft_strcmp("echo", curr->argv[0]) == 0)
+		return (1);
+	else if (ft_strcmp("env", curr->argv[0]) == 0)
+		return (1);
+	else if (ft_strcmp("pwd", curr->argv[0]) == 0)
+		return (1);
+	else if ((ft_strcmp("export", curr->argv[0]) == 0) && curr->argc == 1)
+		return (1);
+	return (0);
+}
+
+int				check_shell_builtin(t_cmd *curr)
+{
+	if (ft_strcmp("exit", curr->argv[0]) == 0)
+		return (1);
+	else if ((ft_strcmp("export", curr->argv[0]) == 0) && curr->argc > 1)
+		return (1);
+	else if (ft_strcmp("unset", curr->argv[0]) == 0)
+		return (1);
+	else if (ft_strcmp("cd", curr->argv[0]) == 0)
+		return (1);
+	return (0);
+}
+void			builtin_cmd(t_cmd *curr)
+{
+	if (ft_strcmp("exit", curr->argv[0]) == 0)
+		ft_exit(curr);
+	else if (ft_strcmp("export", curr->argv[0]) == 0)
+		ft_export(curr);
+	else if (ft_strcmp("unset", curr->argv[0]) == 0)
+		ft_unset(curr);
+	else if (ft_strcmp("cd", curr->argv[0]) == 0)
+		ft_cd(curr);
+}
+
+static void		find_builtin_cmd(t_cmd *curr)
+{
+	if (ft_strcmp("echo", curr->argv[0]) == 0)
+		ft_echo(curr);
+	else if (ft_strcmp("env", curr->argv[0]) == 0)
+		ft_env();
+	else if (ft_strcmp("pwd", curr->argv[0]) == 0)
+		ft_pwd();
+		else if (ft_strcmp("export", curr->argv[0]) == 0)
+		ft_export_fork(curr);
+}
+
+void			builtin_cmd_fork(t_cmd *curr)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		check_pipe(curr);
+		if (curr->fd_in != 0)
+			dup2(curr->fd_in, 0);
+		if (curr->fd_out != 1)
+			dup2(curr->fd_out, 1);
+		find_builtin_cmd(curr);
+		exit(g_data.return_value);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		close(curr->pipe[1]);
+	}
+}
