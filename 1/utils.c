e8faa38627289fa6c111a4d0b0f24f0d66d74e6f
from97 <skm1104@daum.net>
2021-06-16 16:51:48 +0900

    strtrim free 부분 제거 -> segfault 해결

--- 1/utils.c
+++ 1/utils.c
@@ -6,7 +6,7 @@
 /*   By: toh <toh@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 14:39:24 by toh               #+#    #+#             */
-/*   Updated: 2021/06/15 20:54:45 by seomoon          ###   ########.fr       */
+/*   Updated: 2021/06/16 16:51:32 by seomoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,7 +133,6 @@ char		*ft_strtrim(char *str)
 	if (!result)
 		return (NULL);
 	ft_strlcpy(result, (char *)(str + begin), end + 1);
-	free(str);
 	return (result);
 }
 
